31faffeb78faaefba80a7ddf733ffcad733a8920
btjansen <btjansen@asu.edu>
2017-05-02 17:40:15 -0600

    Update InRangeTest.java

--- test/junit/InRangeTest.java
+++ test/junit/InRangeTest.java
@@ -11,26 +11,31 @@ public class InRangeTest {
 	public InRangeTest() {
 	}
 
+	//values both in range
 	@Test
 	public void inRangeTest1() {
 		assertTrue(JunitTester.inRangeTest(0, 7));
 	}
 	
+	//first value below range
 	@Test
 	public void inRangeTest2() {
 		assertFalse(JunitTester.inRangeTest(-1, 7));
 	}
 	
+	//first value above range
 	@Test
 	public void inRangeTest3() {
 		assertFalse(JunitTester.inRangeTest(8, 7));
 	}
 	
+	//second value below range
 	@Test
 	public void inRangeTest4() {
 		assertFalse(JunitTester.inRangeTest(0, -1));
 	}
 
+	//second value above range
 	@Test
 	public void inRangeTest5() {
 		assertFalse(JunitTester.inRangeTest(0, 8));
